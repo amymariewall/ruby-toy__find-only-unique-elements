d63db4b0ad7fbdb28ed042eb2eb2fcd29124ab31
amymariewall <amymariewall@gmail.com>
2016-01-21 19:19:47 +0000

    Exercise Unique Elements 2

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,15 +2,13 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
- new_array=[]
- unique_array=arr.uniq
-      unique_array.each do |x| 
-      new_array << [arr.count(x), x]
+    new_array=[]
+    arr.each do |i| 
+        arr.count(i)
+        if arr.count(i)==1
+            new_array << i
+        end
     end
-    newer_array = new_array.select {|x| x[0] == 1}
-    final_array=[]
-    newer_array.each do |x|
-        final_array << x.pop
-    end
-    return final_array
+        return new_array
 end
+
