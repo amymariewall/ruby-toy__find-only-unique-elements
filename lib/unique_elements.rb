948f2c02f06d6d3b1fff25d5dad03667b079d2b7
amymariewall <amymariewall@gmail.com>
2016-01-21 19:06:55 +0000

    Occur Only Once Response

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,15 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+ new_array=[]
+ unique_array=arr.uniq
+      unique_array.each do |x| 
+      new_array << [arr.count(x), x]
+    end
+    newer_array = new_array.select {|x| x[0] == 1}
+    final_array=[]
+    newer_array.each do |x|
+        final_array << x.pop
+    end
+    return final_array
 end
